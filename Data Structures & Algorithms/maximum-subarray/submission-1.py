d057702c1ce74cde92467b53b93cb8c825649e0d
Henry Park <codedenma@gmail.com>
2026-04-03 09:46:03 -0700

    Update: maximum-subarray - submission-1

--- Data Structures & Algorithms/maximum-subarray/submission-1.py
+++ Data Structures & Algorithms/maximum-subarray/submission-1.py
@@ -1,5 +1,6 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
+        # O(N) Time | O(1) Space
         res = nums[0]
         max_so_far = 0
 
@@ -7,9 +8,9 @@ class Solution:
             max_so_far += num
 
             res = max(res, max_so_far)
-            
+
             if max_so_far < 0:
                 max_so_far = 0
 
         return res
-        
+
