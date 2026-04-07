061f34f88f04ab54bb30d0ee572e8ddbf715435a
Henry Park <codedenma@gmail.com>
2026-04-07 15:11:30 -0700

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.py
@@ -0,0 +1,23 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        if not intervals:
+            return True
+
+        intervals.sort(key=lambda x: x.start)
+
+        for i in range(len(intervals) - 1):
+            end = intervals[i].end
+            next_start = intervals[i + 1].start
+
+            if end > next_start:
+                return False
+
+        return True
