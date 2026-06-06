131b3d0aa914fc44a9119bc14d8ce820ad320ecf
Henry Park <codedenma@gmail.com>
2026-06-05 23:02:36 -0700

    Add: set-zeroes-in-matrix - submission-3

---
+++ Data Structures & Algorithms/set-zeroes-in-matrix/submission-3.py
@@ -0,0 +1,54 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        '''
+        will keep track of which row/col to zero out from the first
+        row and column
+
+        matrix[0][0] will indicate whether or not to zero out the first
+        col
+
+        to determine zeroing out first row, we'll use a boolean
+
+        go through rows
+            go through cols
+                if cell is 0
+                    if it's first row
+                        set zero_first_row to true
+                    
+                    matrix[row][0] = 0
+                    matrix[0][col] = 0
+
+        go through inner cells
+
+        go through first row
+
+        go through first col
+        '''
+        m, n = len(matrix), len(matrix[0])
+        zero_first_row = False
+
+        for row in range(m):
+            for col in range(n):
+                if matrix[row][col] == 0:
+                    if row == 0:
+                        zero_first_row = True
+                    else:
+                        matrix[row][0] = 0
+                        matrix[0][col] = 0
+
+        for row in range(1, m):
+            for col in range(1, n):
+                if matrix[0][col] == 0 or matrix[row][0] == 0:
+                    matrix[row][col] = 0
+
+        if matrix[0][0] == 0:
+            for row in range(m):
+                matrix[row][0] = 0
+
+        if zero_first_row:
+            for col in range(n):
+                matrix[0][col] = 0
+
+
+        
+        
